6fff839c9d830b1ea54c83a2e658466c1638c6bc
fadix1234 <ahmadzaiyasmin@yahoo.com>
2023-09-13 00:48:35 -0700

    first

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,4 @@
 // Write your "projects" router here!
+server.get('/api/projects', (req, res) => {
+    res.status(200).send('');
+  });
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+require('dotenv').config();
+
+const server = require('./api/server.js');
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+  console.log(`\n***Server Running on http://localhost:${port} ***\n`);
+});
+
+
 /*
 play this: https://www.youtube.com/watch?v=d-diB65scQU
 
