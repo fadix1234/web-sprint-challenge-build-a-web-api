ea24f712f284fd0be96338a7a64dcfa2838f1115
fadix1234 <ahmadzaiyasmin@yahoo.com>
2023-09-14 11:23:48 -0700

    first

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,19 +2,30 @@
 const express = require('express');
 
 const router = express.Router();
+// router.get('/', (req, res) => {
+//     res.send(`Hello Projects`);
+// });
+const Projects = require('./projects-model');
 
-router.get('/api/projects', (req, res) => {
-    if (!projects) {
-        res.status(500).json([]);
-    } else {
-        res.status(200).json(projects);
-    }
+router.get('/', (req, res) => {
+    Projects.get().then(projects => res.status(200).json(projects)).catch(err => console.log(err))
+
+
 });
 
-
-router.get('/api/projects/:id', (req, res) => {
-    const id = req.params.id;
-    res.status(200).send('404');
+router.get('/:id', (req, res) => {
+    Projects.get(req.params.id).then(project => {
+        if (project) {
+            res.status(200).json(project);
+        } else {
+            res.status(404).json({ error: 'Project not found' });
+        }
+    })
 });
+
+router.post('./projects', (req, res) => {
+    res.status(201).json({ url: '/', operation: 'POST' });
+});
+
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const server = express();
+const projectsRouter = require('./projects/projects-router');
 
 server.use(express.json())
+
+server.use('/api/projects', projectsRouter)
 
 server.get('/', (req, res) => {
     res.send(`Hello Express`);
 });
 
+server.post('/', (req, res) => {
+    const newProjects = req.body;
+    newProjects.id = nextId++;
+  
+    projects.push(newProjects);
+  
+    res.status(201).json(newProjects);
+  });
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
